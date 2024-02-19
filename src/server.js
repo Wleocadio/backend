5be33587556aa6cbde31a87c9bd5f13dddeb837a
Wleocadio <wleocadi@gmail.com>
2024-02-19 19:07:23 -0300

    adicionado cors

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 require('dotenv').config();
+const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 3000;
 const mongoose = require('mongoose');
@@ -55,6 +56,11 @@ async function run() {
 }
 run().catch(console.dir);
 */
+app.use(cors({
+  // Permitir que todos os domínios acessem sua API
+  // Para produção, você pode querer restringir isso a domínios específicos
+  origin: '*'
+}));
 app.use(express.json());
 
 // Agendar tarefa para executar diariamente em um horário específico (por exemplo, à meia-noite)
